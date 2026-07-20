a5abf34e8c8b104424da39697f9d0c802d6685e7
ifelsewhat <eittaiwo1@gmail.com>
2026-07-20 11:33:22 +0200

    Added basic form handling and added typwriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-//alert("Hello World");
+function generateQuote(event) {
+  event.preventDefault();
+
+  new Typewriter("#quote", {
+    strings: "The world will ask you who you are",
+    autoStart: true,
+  });
+  quoteElement.innerHTML = "The world will ask you who you are";
+}
+
+let philosophyElement = document.querySelector("#philosophy-quote-generator");
+
+philosophyElement.addEventListener("submit", generateQuote);
